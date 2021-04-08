6146568ef182069f9eeec97b19ec18d03afc6fd0
ralphbartley <ralphgbartley@gmail.com>
2021-04-08 17:39:11 +0100

    interface file with jquery

--- thermostat/jasmine/src/interface.js
+++ thermostat/jasmine/src/interface.js
@@ -4,6 +4,36 @@
 
     $( "#mode" ).click(function( event ) {
       thermostat.changeMode();
-      console.log(thermostat.getPowerSave());
+      $('#power_mode').text(thermostat.getPowerSave())
     });
+
+    $( "#reset" ).click(function( event ) {
+      thermostat.reset();
+      updateTemperature();
+
+    });
+
+    $( "#up" ).click(function( event ) {
+      thermostat.up();
+      updateTemperature();
+    });
+
+    $( "#down" ).click(function( event ) {
+      thermostat.down();
+      updateTemperature();
+    });
+
+    let updateTemperature = ( ) => {
+      $('#temperature').text(thermostat.temperature);
+      if(thermostat.energyUse() === "low-usage"){
+        $('.dot').css("background-color", "green");
+      }
+      else if(thermostat.energyUse() === "medium-usage"){
+        $('.dot').css("background-color", "black");
+      }
+      else{
+        $('.dot').css("background-color", "red");
+      }
+    }
+
   });
